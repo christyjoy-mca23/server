60760e802e5492c63c64b6442f2a2e1f47626a32
christy joy <139836162+christyjoy-mca23@users.noreply.github.com>
2024-03-21 12:39:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,70 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors=require('cors')
-const bodyParser = require('body-parser')
-const dotenv=require('dotenv').config()
-const PORT=process.env.PORT
-const URL=process.env.URL
+const express = require('express');
+const mongoose=require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+const app = express();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/', function (req, res){
-  res.setHeader("Access-Control-Allow-Credentials","true")
-  res.send('welcome')
+app.get('/',(req,res)=>{
+    res.send("<h1>Welcome</h1>")
 })
 
-//fetch (get method)//
-app.get('/data',function(req,res) {Data.find().then((item)=> res.send(item))})
-
-//post//
-app.post('/create', function(req,res) {Data.create(req.body).then((item)=> res.send(item))})
-
-app.listen(PORT, () => {
-  console.log("server is connected")
+app.get('/data',(req,res)=>{
+   Data.find().then((item)=>res.send(item))
 })
 
-mongoose.connect(URL).then(()=>console.log("mongoDB is Connected"))
-
-//update//
-app.put("/update/:id",function(req,res){
-  Data.findByIdAndUpdate({_id:req.params.id},req.body,{new:"true"}).then((item)=>res.send(item))
-})
-
-//delete//
-app.delete("/delete/:id",function(req,res){
+app.post('/create',(req,res)=>{
+    // console.log(req.body);
+    Data.create(req.body).then((item)=>res.send(item))
+ })
  
-  Data.findByIdAndDelete({_id:req.params.id}).then((item)=>res.send(item))
-})
+ app.put('/update/:id',(req,res)=>{
+    console.log(req.body);
+    console.log(req.params.id);
+    Data.findOneAndUpdate({_id:req.params.id},{$set:req.body},{ new: true }).then(res => console.log(res))
+    
+ })
+
+ app.delete('/delete/:id',(req,res)=>{
+        Data.findOneAndDelete({_id:req.params.id}).then(response => console.log(response))
+    })
+
+app.listen(8080,()=>{
+    console.log("server started !!!");
+});
+
+// connect mongodb
+
+// mongoose.connect("mongodb+srv://nivashini:nivashini@cluster0.9pl1wha.mongodb.net/mca").then(()=>{console.log("MongoDB connected!")})
+mongoose.connect("mongodb+srv://Alamelumangai:Alamelumangai@cluster0.myuphpq.mongodb.net/Rvs").then(console.log("MongoDB connected"))
+
+// create a schema
+
+let newSchema = new mongoose.Schema({
+                 name : String,
+                 email:String,
+                 password : String,
+                 amount : Number
+                })
+
+// model
+let Data=mongoose.model('Rvs',newSchema)
+// let Data = mongoose.model('mca' , newSchema)
+
+// // create a data for testing
+
+// let data1 = new Data({
+//     name :"Rehana Bensha",
+//     email:"rehana@gmail.com",
+//     password:"12345789",
+//     amount : 20000
+// })
+
+// data1.save();
 
 
 
-
-//create a schema//
-var newSchema = new mongoose.Schema({
-
-     name:String,
-     email:String,
-     password:String,
-     amount:Number
-})
-
-//model//
-
-let Data = mongoose.model('mca',newSchema)
